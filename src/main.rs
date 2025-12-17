334a160d02e8b9b20b64ba3d901d1ab2acdbce0a
Zod1s <lorefrance15@gmail.com>
2025-12-17 15:28:11 +0100

    nuove cose da fare

--- src/main.rs
+++ src/main.rs
@@ -12,61 +12,61 @@ mod wiener;
 use crate::utils::*;
 
 fn main() -> utils::SolverResult<()> {
-    examples::actualfeed::actualfeed()
+    // examples::actualfeed::actualfeed()
     // examples::parallel::parallel()
-    // let id = na::Matrix2::<na::Complex<f64>>::identity();
-    // let s1 = PAULIS
-    //     .iter()
-    //     .map(|pauli| pauli.kronecker(&id).kronecker(&id))
-    //     .collect::<Vec<Operator<na::U8>>>();
-    //
-    // let s2 = PAULIS
-    //     .iter()
-    //     .map(|pauli| id.kronecker(&pauli).kronecker(&id))
-    //     .collect::<Vec<Operator<na::U8>>>();
-    //
-    // let s3 = PAULIS
-    //     .iter()
-    //     .map(|pauli| id.kronecker(&id).kronecker(&pauli))
-    //     .collect::<Vec<Operator<na::U8>>>();
-    //
-    // // let s4 = PAULIS
-    // //     .iter()
-    // //     .map(|pauli| id.kronecker(&id).kronecker(&id).kronecker(&pauli))
-    // //     .collect::<Vec<Operator<na::Const<16>>>>();
-    //
-    // let weights = vec![0.5, 0.5, 3.0];
-    // let ferromag = -1.;
-    // let hm = 0.0;
-    //
-    // let h = -s1
-    //     .iter()
-    //     .zip(s2.iter())
-    //     .zip(weights.iter())
-    //     .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
-    //     .sum::<Operator<na::U8>>()
-    //     - s2.iter()
-    //         .zip(s3.iter())
-    //         .zip(weights.iter())
-    //         .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
-    //         .sum::<Operator<na::U8>>()
-    //     - s3.iter()
-    //         .zip(s1.iter())
-    //         .zip(weights.iter())
-    //         .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
-    //         .sum::<Operator<na::U8>>()
-    //     - (s1[2] + s2[2] + s3[2]).scale(hm);
-    // // + s4.iter()
-    // //     .zip(s1.iter())
-    // //     .zip(weights.iter())
-    // //     .map(|((p1, p2), w)| p1 * p2.scale(*w))
-    // //     .sum::<Operator<na::Const<16>>>();
-    //
-    // println!("H: {}", h.map(|a| a.re));
-    // let eigen = h.symmetric_eigen();
-    // println!("Eigenvalues: {:.4}", eigen.eigenvalues);
-    // println!("Sum of eigenvalues: {:.4}", eigen.eigenvalues.sum());
-    // println!("Eigenvectors: {:.4}", eigen.eigenvectors.map(|v| v.re));
-    //
-    // Ok(())
+    let id = na::Matrix2::<na::Complex<f64>>::identity();
+    let s1 = PAULIS
+        .iter()
+        .map(|pauli| pauli.kronecker(&id).kronecker(&id).kronecker(&id))
+        .collect::<Vec<Operator<na::U16>>>();
+
+    let s2 = PAULIS
+        .iter()
+        .map(|pauli| id.kronecker(&pauli).kronecker(&id).kronecker(&id))
+        .collect::<Vec<Operator<na::U16>>>();
+
+    let s3 = PAULIS
+        .iter()
+        .map(|pauli| id.kronecker(&id).kronecker(&pauli).kronecker(&id))
+        .collect::<Vec<Operator<na::U16>>>();
+
+    let s4 = PAULIS
+        .iter()
+        .map(|pauli| id.kronecker(&id).kronecker(&id).kronecker(&pauli))
+        .collect::<Vec<Operator<na::U16>>>();
+
+    let weights = vec![1., 1., 1.0];
+    let ferromag = -1.;
+    let hm = 2.0;
+
+    let h = -s1
+        .iter()
+        .zip(s2.iter())
+        .zip(weights.iter())
+        .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
+        .sum::<Operator<na::U16>>()
+        - s2.iter()
+            .zip(s3.iter())
+            .zip(weights.iter())
+            .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
+            .sum::<Operator<na::U16>>()
+        - s3.iter()
+            .zip(s4.iter())
+            .zip(weights.iter())
+            .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
+            .sum::<Operator<na::U16>>()
+        - (s1[2] + s2[2] + s3[2] + s4[2]).scale(hm)
+        - s4.iter()
+            .zip(s1.iter())
+            .zip(weights.iter())
+            .map(|((p1, p2), w)| p1 * p2.scale(ferromag * w))
+            .sum::<Operator<na::U16>>();
+
+    println!("H: {}", h.map(|a| a.re));
+    let eigen = h.symmetric_eigen();
+    println!("Eigenvalues: {:.4}", eigen.eigenvalues);
+    println!("Sum of eigenvalues: {:.4}", eigen.eigenvalues.sum());
+    println!("Eigenvectors: {:.4}", eigen.eigenvectors.map(|v| v.re));
+
+    Ok(())
 }
